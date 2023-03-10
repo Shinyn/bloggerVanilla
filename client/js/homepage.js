93c844d479ae3a2206d9fdcd669905417fc12239
Shinyn <mattias.alm@hotmail.com>
2023-03-10 16:05:14 +0100

    fixing homepage

--- client/js/homepage.js
+++ client/js/homepage.js
@@ -1,23 +1,28 @@
 "use strict";
 const input = document.querySelector("#inputField");
 document.querySelector("#openTaskBtn").addEventListener("click", toogle);
-const taskWindow = document.querySelector("#task-form");
+const taskWindow = document.querySelector("#taskForm");
 taskWindow.style.display = "none";
 
 taskWindow.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("adbgu in");
-
   const html = document.createElement("div");
-
+  html.id = "taskMainDiv";
   // ersätt med data vi får från databasen
   html.innerHTML = `
     <div class="taskDiv">
-    <p class="task-text">Test text</p>
-    <button class="task-btn">Delete</button>
+    <p class="taskText">${input.value}</p>
+    <button class="taskBtn">Delete</button>
     </div>`;
   document.body.appendChild(html);
+  document.querySelector(".taskBtn").addEventListener("click", destroyElement);
+  // ska göra POST till servern - kolla så det är rätt användare osv.
 });
+
+function destroyElement() {
+  console.log("Yes");
+  // här ska vi köra en DELETE
+}
 
 function toogle() {
   if (taskWindow.style.display === "none") {
